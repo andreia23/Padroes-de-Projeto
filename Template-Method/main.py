21a921d98b954361206190317e31822623c765d4
andreia23 <44982439+andreia23@users.noreply.github.com>
2021-06-04 00:49:37 -0300

    Padrão Template-Method

---
+++ Template-Method/main.py
@@ -0,0 +1,23 @@
+import AbstractClass
+from ConcreteClass1 import ConcreteClass1
+from ConcreteClass2 import ConcreteClass2
+
+def client_code(abstract_class: AbstractClass) -> None:
+    """
+    The client code calls the template method to execute the algorithm. Client
+    code does not have to know the concrete class of an object it works with, as
+    long as it works with objects through the interface of their base class.
+    """
+
+    # ...
+    abstract_class.template_method()
+    # ...
+
+
+if __name__ == "__main__":
+    print("Same client code can work with different subclasses:")
+    client_code(ConcreteClass1())
+    print("")
+
+    print("Same client code can work with different subclasses:")
+    client_code(ConcreteClass2())
