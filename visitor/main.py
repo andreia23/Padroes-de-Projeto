2c6cb5ebe0e23eb063eaa266c315cd33b6794f51
HelenaJuliana <helenajuliana14@gmail.com>
2021-06-03 22:24:49 -0300

    Ajustes do visitor com remoção de arquivos desnecessários

--- visitor/main.py
+++ visitor/main.py
@@ -4,37 +4,72 @@ from typing import List
 
 
 class Component(ABC):
-    """
-    The Component interface declares an `accept` method that should take the
-    base visitor interface as an argument.
-    """
-
+  
     @abstractmethod
     def accept(self, visitor: Visitor) -> None:
         pass
 
 
 class ConcreteComponentA(Component):
-    """
-    Each Concrete Component must implement the `accept` method in such a way
-    that it calls the visitor's method corresponding to the component's class.
-    """
-
+   
     def accept(self, visitor: Visitor) -> None:
-        """
-        Note that we're calling `visitConcreteComponentA`, which matches the
-        current class name. This way we let the visitor know the class of the
-        component it works with.
-        """
-
+        
         visitor.visit_concrete_component_a(self)
 
     def exclusive_method_of_concrete_component_a(self) -> str:
-        """
-        Concrete Components may have special methods that don't exist in their
-        base class or interface. The Visitor is still able to use these methods
-        since it's aware of the component's concrete class.
-        """
-
         return "A"
 
+
+class ConcreteComponentB(Component):
+   
+    def accept(self, visitor: Visitor):
+        visitor.visit_concrete_component_b(self)
+
+    def special_method_of_concrete_component_b(self) -> str:
+        return "B"
+
+
+class Visitor(ABC):
+
+    @abstractmethod
+    def visit_concrete_component_a(self, element: ConcreteComponentA) -> None:
+        pass
+
+    @abstractmethod
+    def visit_concrete_component_b(self, element: ConcreteComponentB) -> None:
+        pass
+
+
+class ConcreteVisitor1(Visitor):
+    def visit_concrete_component_a(self, element) -> None:
+        print(f"{element.exclusive_method_of_concrete_component_a()} + ConcreteVisitor1")
+
+    def visit_concrete_component_b(self, element) -> None:
+        print(f"{element.special_method_of_concrete_component_b()} + ConcreteVisitor1")
+
+
+class ConcreteVisitor2(Visitor):
+    def visit_concrete_component_a(self, element) -> None:
+        print(f"{element.exclusive_method_of_concrete_component_a()} + ConcreteVisitor2")
+
+    def visit_concrete_component_b(self, element) -> None:
+        print(f"{element.special_method_of_concrete_component_b()} + ConcreteVisitor2")
+
+
+def client_code(components: List[Component], visitor: Visitor) -> None:
+    
+    
+    for component in components:
+        component.accept(visitor)
+    
+
+if __name__ == "__main__":
+    components = [ConcreteComponentA(), ConcreteComponentB()]
+
+    print("The client code works with all visitors via the base Visitor interface:")
+    visitor1 = ConcreteVisitor1()
+    client_code(components, visitor1)
+
+    print("It allows the same client code to work with different types of visitors:")
+    visitor2 = ConcreteVisitor2()
+    client_code(components, visitor2)
